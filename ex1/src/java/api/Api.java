8d5bbb4c8acc29224928ed0c5599a9c74159cf06
JoachimDittman <JoachimDittman@192.168.0.14>
2015-02-27 12:32:01 +0100

    AllPlayersFromWebsite

--- ex1/src/java/api/Api.java
+++ ex1/src/java/api/Api.java
@@ -20,6 +20,13 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import Models.Player;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * REST Web Service
@@ -31,16 +38,25 @@ public class Api {
  List<Player> footballLinks = new ArrayList();
     @Context
     private UriInfo context;
+    private Object rand;
 
     /**
      * Creates a new instance of Api
      */
     public Api() {
-        footballLinks.add(new Player(1,"James Rodríguez","Columbia")); 
-        footballLinks.add(new Player(2,"Thomas Mueller","USA")); 
-        footballLinks.add(new Player(3,"Messi","Sweden")); 
-        footballLinks.add(new Player(4,"Neymar","Demnark")); 
-        footballLinks.add(new Player(5,"van Persie","Australia")); 
+        String arrayFirstName[] ={"James","Thomas","Messi","Neymar","Van"};
+        String arrayLastName[] ={"Rodríguez","Mueller","Tanros","Bullik","Perise"};
+        String arrayCountry[] ={"Denmark","USA","Columbia","Australia","Sweden"};
+        String create1 = "";
+        Random rand = new Random();
+        for (int i = 1; i < 100; i++) {
+            int  x = rand.nextInt(4);
+            int  y = rand.nextInt(4);
+            int  aa = rand.nextInt(4);
+            create1 = ""+arrayFirstName[x]+" "+arrayLastName[y];
+            footballLinks.add(new Player(i,create1,arrayCountry[aa]));   
+        }
+       
     }
 
     /**
@@ -72,6 +88,24 @@ public class Api {
                 return gson.toJson(clubsJson);
         
               
+          
+    }
+         @GET
+      @Path("/AllPlayersFromWebsite")
+     @Produces("application/json")
+    public String getAllPlayersFromWebsite(@PathParam("id") int id) throws IOException {
+            URL url = null; try {
+         url = new URL("http://footballpool.dataaccess.eu/data/info.wso/AllPlayerNames/JSON/debug?bSelected=sName");
+     } catch (MalformedURLException ex) {
+         Logger.getLogger(Api.class.getName()).log(Level.SEVERE, null, ex);
+     }
+URLConnection con = url.openConnection(); Scanner scan = new Scanner(con.getInputStream()); String jsonStr=null;
+if (scan.hasNext()) {
+  jsonStr = scan.nextLine();
+}
+System.out.println(jsonStr);
+scan.close();
+     return jsonStr;
           
     }
     /**
